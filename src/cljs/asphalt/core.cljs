c9e34a40219354789ff7931545a7db85b507ddea
Michael Drogalis <mjd3089@rit.edu>
2013-09-25 19:28:56 -0400

    Revert.

--- src/cljs/asphalt/core.cljs
+++ src/cljs/asphalt/core.cljs
@@ -1,8 +1,9 @@
-(ns asphalt.core)
+(ns asphalt.core
+  (:require [cljs.reader :refer [read-string]]))
 
-(def uri "ws://localhost:9090")
+(def uri "ws://localhost:9090/rush-hour/streaming/edn")
 
-(def ws (js/$.websocket. ws))
+(def ws (js/WebSocket. uri))
 
 (def open-fn
   (fn [] (.log js/console "Connection open. Rock on.")))
@@ -10,8 +11,8 @@
 (def receive-fn
   (fn [message]
     (.log js/console "Received message:")
-    (.log js/console (:snapshot (read-string (.-data message))))))
+    (.log js/console (clj->js (:snapshot (read-string (.-data message)))))))
 
 (set! (.-onopen ws) open-fn)
-(set! (.-onmessage ws) message-fn)
+(set! (.-onmessage ws) receive-fn)
 
