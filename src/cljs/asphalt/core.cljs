6f4aa202216476f889c757f5f08416bac5455c7f
Michael Drogalis <mjd3089@rit.edu>
2013-09-27 19:37:22 -0400

    Map pairs.

--- src/cljs/asphalt/core.cljs
+++ src/cljs/asphalt/core.cljs
@@ -20,10 +20,45 @@
 (def open-fn
   (fn [] (.log js/console "Connection open. Rock on.")))
 
+(defn reverse-links [quad]
+  (.log js/console "Linking.")
+  (js/$.ajax
+   (clj->js
+    {:type "POST"
+     :url "http://localhost:9091/rush-hour/api/reverse-links/edn"
+     :contentType "application/edn"
+     :success (fn [response & _] (.log js/console (clj->js (read-string response))))
+     :data (pr-str quad)
+     :processData false}))
+  [])
+
+(defn ingress-pairing [quad driver]
+  {:src (first (reverse-links quad))
+   :dst (:dst driver)})
+
+(defn egress-pairing [quad driver]
+  {:src quad
+   :dst (:dst driver)})
+
+(defn ingress-lane-pairs [quad state]
+  (map (partial ingress-pairing quad) state))
+
+(defn egress-lane-pairs [quad state]
+  (map (partial egress-pairing quad) state))
+
+(defn ingress-all-pairs [ingress-map]
+  (into {} (map (fn [[quad state]] {quad (ingress-lane-pairs quad state)}) ingress-map)))
+
+(defn egress-all-pairs [egress-map]
+  (into {} (map (fn [[quad state]] {quad (egress-lane-pairs quad state)}) egress-map)))
+
 (def receive-fn
   (fn [message]
-    (.log js/console "Received message:")
-    (.log js/console (:snapshot (read-string (.-data message))))))
+    (let [snapshot (:snapshot (read-string (.-data message)))]
+;;      (.log js/console "Received message:")
+      (clj->js (ingress-all-pairs (:ingress snapshot)))
+;;      (.log js/console (clj->js (egress-all-pairs (:egress snapshot))))
+      )))
 
 (set! (.-onopen ws) open-fn)
 (set! (.-onmessage ws) receive-fn)
