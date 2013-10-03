bd2050c1a9af270af7b01ab554a2fa0aee95d71d
Michael Drogalis <mjd3089@rit.edu>
2013-10-02 21:23:52 -0400

    Push to ws.

--- src/clj/asphalt/relay.clj
+++ src/clj/asphalt/relay.clj
@@ -27,18 +27,6 @@
                  {:body (pr-str {:src src :dst dst :gap gap :extender "Philadelphia, PA"})
                   :content-type "application/edn"}))))
 
-;;; (def ws-chan (websocket-client {:url "http://localhost:9090/rush-hour/streaming/edn"}))
-
-(defn receive [snapshot]
-  (go (>! trans-chan snapshot)))
-
-(comment
-  (receive-all (wait-for-result ws-chan) #(receive (:snapshot (read-string %))))
-
-  (while true
-    (go (let [x (<! trans-chan)]
-          (clojure.pprint/pprint x)))))
-
 (defn triangulate-lane [driver src dst]
   (let [road-length (+ (:street.lane.install/length src)
                        (:street.lane.install/length dst))
@@ -71,61 +59,10 @@
               state))
            payload)))
 
-(def data
-  '{:lights
-    {["10th Street" "Chestnut Street"] {?x [:green]},
-     ["9th Street" "Chestnut Street"] {?x [:green]},
-     ["9th Street" "Market Street"] {?x [:green]},
-     ["10th Street" "Market Street"] {?x [:green]}},
-    :ingress
-    {{:lane/name "in",
-      :street/tag "north",
-      :street/name "10th Street",
-      :intersection/of ["10th Street" "Chestnut Street"]}
-     [],
-     {:lane/name "in",
-      :street/tag "west",
-      :street/name "Chestnut Street",
-      :intersection/of ["9th Street" "Chestnut Street"]}
-     [{:ripe? false,
-       :dst
-       {:intersection/of ["10th Street" "Chestnut Street"],
-        :street/name "Chestnut Street",
-        :street/tag "east",
-        :lane/name "out"},
-       :front 65,
-       :len 1,
-       :id "Mike",
-       :buf 0}],
-     {:lane/name "in",
-      :street/tag "south",
-      :street/name "9th Street",
-      :intersection/of ["9th Street" "Market Street"]}
-     [],
-     {:lane/name "in",
-      :street/tag "east",
-      :street/name "Market Street",
-      :intersection/of ["10th Street" "Market Street"]}
-     []},
-    :egress
-    {{:lane/name "out",
-      :street/tag "east",
-      :street/name "Chestnut Street",
-      :intersection/of ["10th Street" "Chestnut Street"]}
-     [],
-     {:lane/name "out",
-      :street/tag "north",
-      :street/name "9th Street",
-      :intersection/of ["9th Street" "Chestnut Street"]}
-     [],
-     {:lane/name "out",
-      :street/tag "west",
-      :street/name "Market Street",
-      :intersection/of ["9th Street" "Market Street"]}
-     [],
-     {:lane/name "out",
-      :street/tag "south",
-      :street/name "10th Street",
-      :intersection/of ["10th Street" "Market Street"]}
-     []}})
+(def ws-chan (websocket-client {:url "http://localhost:9090/rush-hour/streaming/edn"}))
 
+(defn receive [snapshot]
+  (concat (ingress-coordindates (:ingress snapshot))
+          (egress-coordinates (:egress snapshot))))
+
+(receive-all (wait-for-result ws-chan) #(receive (:snapshot (read-string %))))
